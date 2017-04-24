87ff1d62d76b4e6798078a42f2d83d53cf906089
han0ah <han0ah92@gmail.com>
2017-04-24 21:11:23 +0900

    REtoDFA 작업중 전송

--- REtoDFA.py
+++ REtoDFA.py
@@ -1,5 +1,22 @@
 from REtoNFA import EpsilonNFA, getNFAfromRegExpStr
 
+class DFA():
+    def __init__(self):
+        self.num_of_state = 0
+        self.delta_transition = []
+        self.initial_state = 0
+        self.final_states = []
+        self.state_map_info = [] #
+
+def isSameState(stateList1, stateList2):
+    pass
+
+def getNewStateNumber(setOfStates):
+    pass
+
+def parseNFAtoDFA():
+    pass
+
 regexp = "((a+b)(c+d))*"
 nfa = getNFAfromRegExpStr(regexp)
 
