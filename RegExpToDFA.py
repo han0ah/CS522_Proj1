f348bb34af266b3f238c29d309b2d4957d1bff80
han0ah <han0ah92@gmail.com>
2017-04-24 16:33:30 +0900

    AST -> epsilon-NFA 구현 완료

--- RegExpToDFA.py
+++ RegExpToDFA.py
@@ -15,54 +15,82 @@ class EpsilonNFA():
         elif node.tag == '+':
             self.constructPlus(node)
         elif node.tag == '.':
-            self.constructPlus(node)
+            self.constructConcate(node)
         else:
             self.constructSymbol(node)
 
     # M(E*)
     def constructStar(self, node:NodeInAST):
-        pass
+        NFA1 = EpsilonNFA(node.left_child)
+        self.num_of_state = NFA1.num_of_state + 2
+        for transition in NFA1.transition_list:
+            self.transition_list.append(
+                (transition[0]+1, transition[1], transition[2]+1)
+            )
+
+        self.transition_list.append((0, 'e', 1))
+        self.transition_list.append((NFA1.final_state+1, 'e', 1))
+        self.transition_list.append((1, 'e', self.num_of_state-1))
+
+        self.final_state = self.num_of_state-1
 
     #M(E+E)
     def constructPlus(self, node:NodeInAST):
-        pass
-
-    #M(E.E)
-    def constructConcate(self, node:NodeInAST):
-        NFA1 = EpsilonNFA()
-        NFA1.construct(node.left_child)
-
-        NFA2 = EpsilonNFA()
-        NFA2.construct(node.left_child)
-
+        NFA1 = EpsilonNFA(node.left_child)
+        NFA2 = EpsilonNFA(node.right_child)
         self.num_of_state = NFA1.num_of_state + NFA2.num_of_state
+
         nfa2StartNum = NFA1.num_of_state
+
         # left child NFA의 transition 추가
         for transition in NFA1.transition_list:
             self.transition_list.append(transition)
         # right child NFA의 transition 추가
         for transition in NFA2.transition_list:
             self.transition_list.append(
-                (transition[0]+nfa2StartNum, transition[1], transition[2]+nfa2StartNum)
+                (transition[0] + nfa2StartNum, transition[1], transition[2] + nfa2StartNum)
             )
-        # left child NFA의 final state를 right child NFA의 start state로 e-move로 연결
-        self.transition_list.append( (NFA1.final_state, 'e', nfa2StartNum) )
 
+        # 위 아래로 epsilon move 추가
+        self.transition_list.append((0, 'e', nfa2StartNum))
+        self.transition_list.append((NFA2.final_state+nfa2StartNum, 'e', NFA1.final_state))
+
+        self.final_state = NFA1.final_state
+
+
+    #M(E.E)
+    def constructConcate(self, node:NodeInAST):
+        NFA1 = EpsilonNFA(node.left_child)
+        NFA2 = EpsilonNFA(node.right_child)
+
+        self.num_of_state = NFA1.num_of_state + NFA2.num_of_state - 1
+        nfa2StartNum = NFA1.num_of_state-1
+        # left child NFA의 transition 추가
+        for transition in NFA1.transition_list:
+            self.transition_list.append(transition)
+        # right child NFA의 transition 추가
+        for transition in NFA2.transition_list:
+            st = (NFA1.final_state) if (transition[0] == 0) else transition[0] + nfa2StartNum
+            en = (NFA1.final_state) if (transition[2] == 0) else transition[2] + nfa2StartNum
+
+            self.transition_list.append((st, transition[1], en))
         self.final_state = NFA2.final_state + nfa2StartNum
 
-
-    #M(a), M(epsilon)
+    #M(a) or M(epsilon)
     def constructSymbol(self, node:NodeInAST):
         self.num_of_state = 2
         self.transition_list.append((0,node.tag,1))
         self.initial_state = 0
         self.final_state = 1
 
-s = "abc"
-getASTfromRegExpStr(s)
-t = EpsilonNFA(NodeInAST('a'))
+regexp = "((a+b)(c+d))*"
+ast = getASTfromRegExpStr(regexp)
+eNFA = EpsilonNFA(ast)
 
-debug = 1
-
-
+'''
+# Test Code for epsilon NFA Construction
+for trans in eNFA.transition_list:
+    print("%d %s %d"%(trans[0], trans[1], trans[2]))
+print(eNFA.final_state)
+'''
 
