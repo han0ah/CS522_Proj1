607d56e632292e0932a3ce6690895487c246dfa5
han0ah <han0ah92@gmail.com>
2017-04-25 15:07:57 +0900

    한글 오토마타 Backspace 삭제 기능 구현 중

--- kor_automata.py
+++ kor_automata.py
@@ -8,6 +8,9 @@ num_delta = 0 # delta 함수 transition 개수
 delta_trans = [] #delta 함수 list of dictionary 형태이다.
 print_buffer = [] # 입력된 symbol로 부터 만든 한글 출력 buffer
 symbol_to_index = {} # symbol을 index로 mapping
+state_list = [] # Automata에서 이동한 상태 목록
+current_character = [] # 현재 만들고 있는 Character
+last_buffer_backup = []
 
 # 유니코드 표의 모음 순서
 # 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
@@ -30,7 +33,7 @@ index_map_to_chongsung = [1,2,3,4,5,6,7,-1,8,9,10,11,12,13,14,15,16,17,-1,18,19,
 def read_automata():
     global delta_trans
     global symbol_to_index
-    f = open('auto_dfa.txt','r', encoding='utf-8')
+    f = open('dfa.txt','r', encoding='utf-8')
     num_symbol = 0
     while(True): # Symbol 목록을 입력받는다. 각 symbol 에게 0부터 할당된 index를 설정한다.
         c = f.read(1)
@@ -56,6 +59,7 @@ def read_automata():
 
 # 화면을 clear 하고 업데이트 한다.
 def update_screen():
+    global current_character
     os.system('cls')
     for character in print_buffer:
         sys.stdout.write(character)
@@ -66,7 +70,9 @@ def update_screen():
 단순히 print_buffer를 하나 늘리고 symbol을 추가해줌.
 '''
 def process_new_symobl_type0(symbol):
+    global current_character
     print_buffer.append(chr(symbol))
+    current_character = print_buffer[-2:] if len(print_buffer) > 1 else print_buffer[-1:]
     return
 
 '''
@@ -74,6 +80,7 @@ def process_new_symobl_type0(symbol):
 현재 버퍼에 모음 symbol 하나를 추가해서 변경 하는 경우이다. 예) ㄱ + ㅏ -> 가 or 고 + ㅐ -> 괘
 '''
 def process_new_symobl_type1(symbol, lastBuffer):
+    global current_character
     moum_hapsung = {8:{}, 13:{}, 18:{}}
     moum_hapsung[8]['ㅏ'] = 'ㅘ'
     moum_hapsung[8]['ㅐ'] = 'ㅙ'
@@ -96,6 +103,8 @@ def process_new_symobl_type1(symbol, lastBuffer):
 
     print_buffer.pop()
     print_buffer.append(chr(newCharacter))
+
+    current_character = print_buffer[-1:]
     return
 
 
@@ -104,6 +113,7 @@ def process_new_symobl_type1(symbol, lastBuffer):
 자음이 있었는데 자음이 또 들어와서 현재 버퍼와 이전 버퍼 두가지를 변경해야 하는 경우이다.
 '''
 def process_new_symobl_type2(symbol, lastBuffer):
+    global current_character
     kyupjaum_hapsung = {1:{}, 4:{}, 8:{}, 17:{}} # 1:ㄱ , 4:ㄴ, 8:ㄹ, 16:ㅂ
     kyupjaum_hapsung[1]['ㅅ'] = 3 # ㄳ
     kyupjaum_hapsung[4]['ㅈ'] = 5 # ㄵ
@@ -133,6 +143,7 @@ def process_new_symobl_type2(symbol, lastBuffer):
     print_buffer.append(chr(newCharacter))
     print_buffer.append(chr(symbol))
 
+    current_character = print_buffer[-2:] if len(print_buffer) > 1 else print_buffer[-1:]
     return
 
 # 새로운 symbol을 처리한다.
@@ -172,6 +183,7 @@ def process_new_symbol(symbol):
 read_automata()
 # 시작스테이트는 0번이다.
 now_state = 0
+state_list.append(0)
 while(True):
     update_screen()
     c = sys.stdin.read(1)
@@ -191,8 +203,6 @@ while(True):
             continue
 
         now_state = delta_trans[now_state][c]
+        state_list.append(now_state)
         process_new_symbol(c)
-
-
-
 
